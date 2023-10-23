0bcd522f2bb3e4e44b75491370f62e369257f3b9
mohamedlajmileanix <mohamed.lajmi@leanix.net>
2023-10-23 13:10:23 +0200

    Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ repositories {
 	mavenCentral()
 }
 
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
